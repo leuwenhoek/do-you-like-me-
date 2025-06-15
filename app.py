31c5d2b55bc6585e2448d07a59fe8c15f85bedba
Ayush kumar <ninjabeastyy24@gmail.com>
2025-06-15 15:44:31 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask,render_template,request,redirect,url_for
+
+app=Flask(__name__)
+
+@app.route("/",methods=["GET","POST"])
+def show():
+    if request.method == "POST":
+        answer = request.form.get("answer")
+        if answer=="yes":
+            return redirect(url_for("yes"))
+        elif answer=="no":
+            return redirect(url_for("no"))
+    
+    return render_template("index.html")
+    
+@app.route("/Iknow")
+def yes():
+    return render_template("i_knew_it.html",message="Toh first message kardke mere baare mee puch lee yaar pls... 🙂")
+
+@app.route("/chalna")
+def no():
+    return render_template("index.html",message="Enter again", no=True)
